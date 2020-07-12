a39152ed4df0239672beedc364de9394474ed09c
fenixfurion <fenixfurion@gmail.com>
2020-07-12 00:51:35 -0500

    ???

---
+++ scenes/input/gameInputManager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# gameInputManager
+# recalculate keybinds in pickupAction and dropAction?
+
+func _ready():
+	pass
+
+func getInputStrength(playerInput):
+	pass
+	
+func _input(event):
+	pass
+
+class playerInput extends Reference:
+	pass
