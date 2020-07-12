a2329b4934a528cd16c8d06a1514d3b9e0c5a609
fenixfurion <fenixfurion@gmail.com>
2020-07-12 14:18:32 -0500

    make render item

--- scenes/items/item_on_ground.gd
+++ scenes/items/item_on_ground.gd
@@ -5,13 +5,10 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var item = ItemManager.get_item_from_list()
-	var text = load("res://art/8BIT CanariPack TopDown/SPRITES/ITEMS/item8BIT_boots.png")
-	
-	draw_texture(text, self.position)
+	$sprite.texture = load("res://art/8BIT CanariPack TopDown/SPRITES/ITEMS/item8BIT_boots.png")
 	
 	pass # Replace with function body.
 
